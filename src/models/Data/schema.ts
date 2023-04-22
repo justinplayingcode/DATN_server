38eb4a8578c1d80b16306aec8673c16268b27398
Justin small hands <phamduythang00@gmail.com>
2023-04-22 14:11:58 +0700

    create model

--- src/models/Data/schema.ts
+++ src/models/Data/schema.ts
@@ -13,6 +13,22 @@ export const collectionName = {
     Posts: "Posts"
 }
 
+export const schemaFields = {
+    username: 'username',
+    password: 'password',
+    _id: '_id',
+    role: 'role',
+    avatar: 'avatar',
+    gender: 'gender',
+    phonenumber: 'phonenumber',
+    address: 'address',
+    speciality: 'speciality',
+    dateOfBirth: 'dateOfBirth',
+    insurance: 'insurance',
+    identification: 'identification',
+    hospitalization: 'hospitalization'
+}
+
 export enum Role {
     doctor,
     patient,
@@ -27,19 +43,20 @@ export enum Gender {
 export enum statusAppointment {
     wait,
     process,
+    testing,
     done
 }
 
 export enum DepartmentType {
-    khamBenh,
-    noiTongHop,
-    ngoai,
-    canLamSang,
-    san,
-    daLieu,
-    dongY,
-    truyenNhiem,
-    duoc,
-    nhi,
-    thanNhanTao
+    khamBenh = "BV00KKB01",
+    noiTongHop = "BV00KTH02",
+    ngoai = "BV00KN03",
+    canLamSang = "BV00KCLS04",
+    san = "BV00KS05",
+    daLieu = "BV00KDL06",
+    dongY = "BV00KDY07",
+    truyenNhiem = "BV00KTN08",
+    duoc = "BV00KD09",
+    nhi = "BV00KN10",
+    thanNhanTao = "BV00KTNT11"
 }
