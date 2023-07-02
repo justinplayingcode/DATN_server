31f9ee6e4370b746714a21e7edf86fbddd138478
justin.pham <phamduythang00@gmail.com>
2023-07-02 15:50:59 +0700

    fix permission in api get doctor in department

--- src/routes/departmentRoute.ts
+++ src/routes/departmentRoute.ts
@@ -8,6 +8,6 @@ const departmentRoute = Router();
 departmentRoute.route('/create').post(Middlewares.verifyToken, Middlewares.permission([Role.admin]),  DepartmentController.createDepartment); //done
 departmentRoute.route('/getall').get(Middlewares.verifyToken, DepartmentController.getAllDepartment); //done
 departmentRoute.route('/getall').post(Middlewares.verifyToken, DepartmentController.getAllForTable); //done
-departmentRoute.route('/getalldoctors').post(Middlewares.verifyToken, Middlewares.permission([Role.patient]), DepartmentController.getAllDoctorsInDepartment) // 
+departmentRoute.route('/getalldoctors').post(Middlewares.verifyToken, DepartmentController.getAllDoctorsInDepartment) // 
 
 export default departmentRoute;
