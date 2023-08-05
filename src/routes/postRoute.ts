8690125293ee7cb2ecc5baf1b1ea9b8cf4850f46
justin.pham <phamduythang00@gmail.com>
2023-08-05 13:53:26 +0700

    statistic base, posts base

---
+++ src/routes/postRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import Middlewares from "../middlewares";
+import { Role } from "../utils/enum";
+import PostsController from "../controllers/postsController";
+
+const postsRoute = Router();
+
+postsRoute.route('/').post(Middlewares.verifyToken, Middlewares.permission([Role.admin, Role.doctor]), PostsController.createPost);
+postsRoute.route('/').put(Middlewares.verifyToken, Middlewares.permission([Role.admin, Role.doctor]), PostsController.editPost);
+postsRoute.route('/approve').put(Middlewares.verifyToken, Middlewares.permission([Role.admin]), PostsController.approve);
+postsRoute.route('/delete').put(Middlewares.verifyToken, Middlewares.permission([Role.admin]), PostsController.delete);
+
+export default postsRoute;
